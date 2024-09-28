f3856b2af855a229c826ba8a8b68483a3491b6b7
Aneel Grewal <aneel.grewal@hotmail.com>
2024-09-28 21:49:28 +0200

    updated /students route using MapGropu(/students)

--- Program.cs
+++ Program.cs
@@ -6,14 +6,17 @@ List<Student> students = new List<Student>(){
     new Student(){Id=3,Name="Pål",DateOfBirth=new DateOnly(1997,8,25),Course="Web Development",Address="0506 Oslo"},
 };
 
+const string GetStudentByIdEndpoint = "GetGameById";
 
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/students", () => students);
+var group = app.MapGroup("/students");
 
-app.MapGet("/students/{id}", (int id) =>
+group.MapGet("/", () => students);
+
+group.MapGet("/{id}", (int id) =>
 {
     var student = students.Find(s => s.Id == id);
     if (student == null)
@@ -21,6 +24,44 @@ app.MapGet("/students/{id}", (int id) =>
         return Results.NotFound();
     }
     return Results.Ok(student);
+}).WithName(GetStudentByIdEndpoint);
+
+group.MapPost("/", (Student student) =>
+{
+    student.Id = students.Max(s => s.Id) + 1;
+    students.Add(student);
+
+    return Results.CreatedAtRoute(GetStudentByIdEndpoint, new { id = student.Id }, student);
 });
+
+group.MapPut("/{id}", (int id, Student updatedStudent) =>
+{
+    Student? student = students.Find(s => s.Id == id);
+
+    if (student == null)
+    {
+        return Results.NotFound();
+    }
+    student.Name = updatedStudent.Name;
+    student.DateOfBirth = updatedStudent.DateOfBirth;
+    student.Course = updatedStudent.Course;
+    student.Address = updatedStudent.Address;
+
+    return Results.NoContent();
+
+});
+
+group.MapDelete("/{id}", (int id) =>
+{
+    Student? student = students.Find(s => s.Id == id);
+    if (student == null)
+    {
+        return Results.NotFound();
+    }
+
+    students.Remove(student);
+    return Results.NoContent();
+});
+
 
 app.Run();

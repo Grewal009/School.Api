7b7c39cc85feeb905656b99d0f87ffe3ef5d8a58
Aneel Grewal <aneel.grewal@hotmail.com>
2024-09-29 22:41:03 +0200

    applied DataAnnotations to Student entity

--- Entities/Student.cs
+++ Entities/Student.cs
@@ -1,11 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace School.Api.Entities;
 
 public class Student
 {
     public int Id { get; set; }
+    [Required]
+    [StringLength(50)]
     public required string Name { get; set; }
+    [Required]
     public DateOnly DateOfBirth { get; set; }
+    [Required]
+    [StringLength(50)]
     public required string Course { get; set; }
+    [Required]
+    [StringLength(100)]
     public required string Address { get; set; }
 
 }

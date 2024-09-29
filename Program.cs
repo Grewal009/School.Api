0a8ca5c4a60bc228f1ed435169b97365bacdd223
Aneel Grewal <aneel.grewal@hotmail.com>
2024-09-30 01:28:17 +0200

    DI using AddSingleton<IStudentRepository, InMemoryStudentsRepository>()

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 using School.Api.Endpoints;
+using School.Api.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddSingleton<IStudentRepository, InMemoryStudentsRepository>();
 
 var app = builder.Build();
 
